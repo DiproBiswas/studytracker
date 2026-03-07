824429147265912c6a62d9cd25589f5271b8a82c
DiproBiswas <diprobiswas02@gmail.com>
2026-03-07 17:47:46 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,382 @@
+let b1_c1 = 0;
+let b1_c2 = 0;
+let b1_c3 = 0;
+let b1_c4 = 0;
+let b1_c5 = 0;
+let b1_c6 = 0;
+let b1_c7 = 0;
+let b1_c8 = 0;
+let b1_c9 = 0;
+let b1_c10 = 0;
+let b1_c11 = 0;
+let b1_c12 = 0;
+let b1_c13 = 0;
+let b1_c14 = 0;
+let b1_c15 = 0;
+let b1_c16 = 0;
+let b1_c17 = 0;
+let b1_c18 = 0;
+let b1_c19 = 0;
+let b1_c20 = 0;
+let b1_c21 = 0;
+let b1_c22 = 0;
+let b1_c23 = 0;
+let b1_c24 = 0;
+let b1_c25 = 0;
+let b1_c26 = 0;
+
+
+
+let b2_c1 = 0;
+let b2_c2 = 0;
+let b2_c3 = 0;
+let b2_c4 = 0;
+let b2_c5 = 0;
+let b2_c6 = 0;
+let b2_c7 = 0;
+let b2_c8 = 0;
+let b2_c9 = 0;
+let b2_c10 = 0;
+let b2_c11 = 0;
+let b2_c12 = 0;
+let b2_c13 = 0;
+let b2_c14 = 0;
+let b2_c15 = 0;
+let b2_c16 = 0;
+let b2_c17 = 0;
+let b2_c18 = 0;
+let b2_c19 = 0;
+let b2_c20 = 0;
+
+
+let e1_c1 = 0;
+let e1_c2 = 0;
+let e1_c3 = 0;
+let e1_c4 = 0;
+let e1_c5 = 0;
+let e1_c6 = 0;
+let e1_c7 = 0;
+let e1_c8 = 0;
+let e1_c9 = 0;
+let e1_c10 = 0;
+let e1_c11 = 0;
+
+
+let e2_c1 = 0;
+let e2_c2 = 0;
+let e2_c3 = 0;
+let e2_c4 = 0;
+let e2_c5 = 0;
+let e2_c6 = 0;
+let e2_c7 = 0;
+let e2_c8 = 0;
+let e2_c9 = 0;
+let e2_c10 = 0;
+let e2_c11 = 0;
+let e2_c12 = 0;
+let e2_c13 = 0;
+let e2_c14 = 0;
+let e2_c15 = 0;
+let e2_c16 = 0;
+
+
+let m1_c1 = 0;
+let m1_c2 = 0;
+let m1_c3 = 0;
+let m1_c4 = 0;
+let m1_c5 = 0;
+let m1_c6 = 0;
+let m1_c7 = 0;
+let m1_c8 = 0;
+let m1_c9 = 0;
+let m1_c10 = 0;
+
+
+let m2_c1 = 0;
+let m2_c2 = 0;
+let m2_c3 = 0;
+let m2_c4 = 0;
+let m2_c5 = 0;
+let m2_c6 = 0;
+let m2_c7 = 0;
+let m2_c8 = 0;
+let m2_c9 = 0;
+let m2_c10 = 0;
+
+
+let p1_c1 = 0;
+let p1_c2 = 0;
+let p1_c3 = 0;
+let p1_c4 = 0;
+let p1_c5 = 0;
+let p1_c6 = 0;
+let p1_c7 = 0;
+let p1_c8 = 0;
+let p1_c9 = 0;
+let p1_c10 = 0;
+
+
+let p2_c1 = 0;
+let p2_c2 = 0;
+let p2_c3 = 0;
+let p2_c4 = 0;
+let p2_c5 = 0;
+let p2_c6 = 0;
+let p2_c7 = 0;
+let p2_c8 = 0;
+let p2_c9 = 0;
+let p2_c10 = 0;
+let p2_c11 = 0;
+
+
+
+let c1_c1 = 0;
+let c1_c2 = 0;
+let c1_c3 = 0;
+let c1_c4 = 0;
+let c1_c5 = 0;
+
+
+let c2_c1 = 0;
+let c2_c2 = 0;
+let c2_c3 = 0;
+let c2_c4 = 0;
+let c2_c5 = 0;
+
+
+let bi1_c1 = 0;
+let bi1_c2 = 0;
+let bi1_c3 = 0;
+let bi1_c4 = 0;
+let bi1_c5 = 0;
+let bi1_c6 = 0;
+let bi1_c7 = 0;
+let bi1_c8 = 0;
+let bi1_c9 = 0;
+let bi1_c10 = 0;
+let bi1_c11 = 0;
+let bi1_c12 = 0;
+
+
+let bi2_c1 = 0;
+let bi2_c2 = 0;
+let bi2_c3 = 0;
+let bi2_c4 = 0;
+let bi2_c5 = 0;
+let bi2_c6 = 0;
+let bi2_c7 = 0;
+let bi2_c8 = 0;
+let bi2_c9 = 0;
+let bi2_c10 = 0;
+let bi2_c11 = 0;
+let bi2_c12 = 0;
+
+
+let ict_c1 = 0;
+let ict_c2 = 0;
+let ict_c3 = 0;
+let ict_c4 = 0;
+let ict_c5 = 0;
+let ict_c6 = 0;
+
+
+
+
+
+
+
+
+
+
+let totalstars_b1 = 130;
+let totalstars_b2 = 100;
+let totalstars_e1 = 65;
+let totalstars_e2 = 80;
+let totalstars_m1 = 50;
+let totalstars_m2 = 50;
+let totalstars_p1 = 100;
+let totalstars_p2 = 110;
+let totalstars_c1 = 50;
+let totalstars_c2 = 50;
+let totalstars_bi1 = 60;
+let totalstars_bi2 = 60;
+let totalstars_ict = 30;
+
+let collectedstars_b1 = b1_c1 + b1_c2 + b1_c3 + b1_c4 + b1_c5 + b1_c6 + b1_c7 + b1_c8 + b1_c9 + b1_c10 +
+                        b1_c11 + b1_c12 + b1_c13 + b1_c14 + b1_c15 + b1_c16 + b1_c17 + b1_c18 + b1_c19 + b1_c20 +
+                        b1_c21 + b1_c22 + b1_c23 + b1_c24 + b1_c25 + b1_c26;
+let collectedstars_b2 = b2_c1 + b2_c2 + b2_c3 + b2_c4 + b2_c5 + b2_c6 + b2_c7 + b2_c8 + b2_c9 + b2_c10 +
+                        b2_c11 + b2_c12 + b2_c13 + b2_c14 + b2_c15 + b2_c16 + b2_c17 + b2_c18 + b2_c19 + b2_c20;
+let collectedstars_e1 = e1_c1 + e1_c2 + e1_c3 + e1_c4 + e1_c5 + e1_c6 + e1_c7 + e1_c8 + e1_c9 + e1_c10 + e1_c11;
+let collectedstars_e2 = e2_c1 + e2_c2 + e2_c3 + e2_c4 + e2_c5 + e2_c6 + e2_c7 + e2_c8 + e2_c9 + e2_c10 +
+                        e2_c11 + e2_c12 + e2_c13 + e2_c14 + e2_c15 + e2_c16;
+let collectedstars_m1 = m1_c1 + m1_c2 + m1_c3 + m1_c4 + m1_c5 + m1_c6 + m1_c7 + m1_c8 + m1_c9 + m1_c10;
+let collectedstars_m2 = m2_c1 + m2_c2 + m2_c3 + m2_c4 + m2_c5 + m2_c6 + m2_c7 + m2_c8 + m2_c9 + m2_c10;
+let collectedstars_p1 = p1_c1 + p1_c2 + p1_c3 + p1_c4 + p1_c5 + p1_c6 + p1_c7 + p1_c8 + p1_c9 + p1_c10;
+let collectedstars_p2 = p2_c1 + p2_c2 + p2_c3 + p2_c4 + p2_c5 + p2_c6 + p2_c7 + p2_c8 + p2_c9 + p2_c10 + p2_c11;
+let collectedstars_c1 = c1_c1 + c1_c2 + c1_c3 + c1_c4 + c1_c5;
+let collectedstars_c2 = c2_c1 + c2_c2 + c2_c3 + c2_c4 + c2_c5;
+let collectedstars_bi1 = bi1_c1 + bi1_c2 + bi1_c3 + bi1_c4 + bi1_c5 + bi1_c6 + bi1_c7 + bi1_c8 + bi1_c9 + bi1_c10 + bi1_c11 + bi1_c12;
+let collectedstars_bi2 = bi2_c1 + bi2_c2 + bi2_c3 + bi2_c4 + bi2_c5 + bi2_c6 + bi2_c7 + bi2_c8 + bi2_c9 + bi2_c10 + bi2_c11 + bi2_c12;
+let collectedstars_ict = ict_c1 + ict_c2 + ict_c3 + ict_c4 + ict_c5 + ict_c6;
+
+
+
+
+let percentage_b1 = (collectedstars_b1 * 100) / totalstars_b1;
+let percentage_b2 = (collectedstars_b2 * 100) / totalstars_b2;
+let percentage_e1 = (collectedstars_e1 * 100) / totalstars_e1;
+let percentage_e2 = (collectedstars_e2 * 100) / totalstars_e2;
+let percentage_m1 = (collectedstars_m1 * 100) / totalstars_m1;
+let percentage_m2 = (collectedstars_m2 * 100) / totalstars_m2;
+let percentage_p1 = (collectedstars_p1 * 100) / totalstars_p1;
+let percentage_p2 = (collectedstars_p2 * 100) / totalstars_p2;
+let percentage_c1 = (collectedstars_c1 * 100) / totalstars_c1;
+let percentage_c2 = (collectedstars_c2 * 100) / totalstars_c2;
+let percentage_bi1 = (collectedstars_bi1 * 100) / totalstars_bi1;
+let percentage_bi2 = (collectedstars_bi2 * 100) / totalstars_bi2;
+let percentage_ict = (collectedstars_ict * 100) / totalstars_ict;
+
+
+let grandtotalstars = 935;
+let grandtotalcollectedstars = collectedstars_b1 + collectedstars_b2 + collectedstars_e1 + collectedstars_e2 +
+                               collectedstars_m1 + collectedstars_m2 + collectedstars_p1 + collectedstars_p2 +
+                               collectedstars_c1 + collectedstars_c2 + collectedstars_bi1 + collectedstars_bi2 +
+                               collectedstars_ict;
+let grandtotal_percentage = (grandtotalcollectedstars * 100) / grandtotalstars
+
+
+
+let pqsel = document.querySelector('.big_percentage')
+let dqsel = document.querySelector('.big_digit')
+let pqsel_b1 = document.querySelector('.big_percentage_b1')
+let dqsel_b1 = document.querySelector('.big_digit_b1')
+let pqsel_b2 = document.querySelector('.big_percentage_b2')
+let dqsel_b2 = document.querySelector('.big_digit_b2')
+let pqsel_e1 = document.querySelector('.big_percentage_e1')
+let dqsel_e1 = document.querySelector('.big_digit_e1')
+let pqsel_e2 = document.querySelector('.big_percentage_e2')
+let dqsel_e2 = document.querySelector('.big_digit_e2')
+let pqsel_m1 = document.querySelector('.big_percentage_m1')
+let dqsel_m1 = document.querySelector('.big_digit_m1')
+let pqsel_m2 = document.querySelector('.big_percentage_m2')
+let dqsel_m2 = document.querySelector('.big_digit_m2')
+let pqsel_p1 = document.querySelector('.big_percentage_p1')
+let dqsel_p1 = document.querySelector('.big_digit_p1')
+let pqsel_p2 = document.querySelector('.big_percentage_p2')
+let dqsel_p2 = document.querySelector('.big_digit_p2')
+let pqsel_c1 = document.querySelector('.big_percentage_c1')
+let dqsel_c1 = document.querySelector('.big_digit_c1')
+let pqsel_c2 = document.querySelector('.big_percentage_c2')
+let dqsel_c2 = document.querySelector('.big_digit_c2')
+let pqsel_bi1 = document.querySelector('.big_percentage_bi1')
+let dqsel_bi1 = document.querySelector('.big_digit_bi1')
+let pqsel_bi2 = document.querySelector('.big_percentage_bi2')
+let dqsel_bi2 = document.querySelector('.big_digit_bi2')
+let pqsel_ict = document.querySelector('.big_percentage_ict')
+let dqsel_ict = document.querySelector('.big_digit_ict')
+
+if(pqsel_b1) {
+    dqsel_b1.textContent = collectedstars_b1 + '/'+ totalstars_b1
+    pqsel_b1.textContent = percentage_b1.toFixed(1) + '%'
+}
+else if(pqsel_b2) {
+    dqsel_b2.textContent = collectedstars_b2 + '/'+ totalstars_b2
+    pqsel_b2.textContent = percentage_b2.toFixed(1) + '%'
+}
+else if(pqsel_e1) {
+    dqsel_e1.textContent = collectedstars_e1 + '/' + totalstars_e1
+    pqsel_e1.textContent = percentage_e1.toFixed(1) + '%'
+}
+else if(pqsel_e2) {
+    dqsel_e2.textContent = collectedstars_e2 + '/' + totalstars_e2
+    pqsel_e2.textContent = percentage_e2.toFixed(1) + '%'
+}
+
+else if(pqsel_m1) {
+    dqsel_m1.textContent = collectedstars_m1 + '/' + totalstars_m1
+    pqsel_m1.textContent = percentage_m1.toFixed(1) + '%'
+}
+
+else if(pqsel_m2) {
+    dqsel_m2.textContent = collectedstars_m2 + '/' + totalstars_m2
+    pqsel_m2.textContent = percentage_m2.toFixed(1) + '%'
+}
+
+else if(pqsel_p1) {
+    dqsel_p1.textContent = collectedstars_p1 + '/' + totalstars_p1
+    pqsel_p1.textContent = percentage_p1.toFixed(1) + '%'
+}
+
+else if(pqsel_p2) {
+    dqsel_p2.textContent = collectedstars_p2 + '/' + totalstars_p2
+    pqsel_p2.textContent = percentage_p2.toFixed(1) + '%'
+}
+
+else if(pqsel_c1) {
+    dqsel_c1.textContent = collectedstars_c1 + '/' + totalstars_c1
+    pqsel_c1.textContent = percentage_c1.toFixed(1) + '%'
+}
+
+else if(pqsel_c2) {
+    dqsel_c2.textContent = collectedstars_c2 + '/' + totalstars_c2
+    pqsel_c2.textContent = percentage_c2.toFixed(1) + '%'
+}
+
+else if(pqsel_bi1) {
+    dqsel_bi1.textContent = collectedstars_bi1 + '/' + totalstars_bi1
+    pqsel_bi1.textContent = percentage_bi1.toFixed(1) + '%'
+}
+
+else if(pqsel_bi2) {
+    dqsel_bi2.textContent = collectedstars_bi2 + '/' + totalstars_bi2
+    pqsel_bi2.textContent = percentage_bi2.toFixed(1) + '%'
+}
+
+else if(pqsel_ict) {
+    dqsel_ict.textContent = collectedstars_ict + '/' + totalstars_ict
+    pqsel_ict.textContent = percentage_ict.toFixed(1) + '%'
+}
+else if(pqsel) {
+    dqsel.textContent = grandtotalcollectedstars + '/' + grandtotalstars
+    pqsel.textContent = grandtotal_percentage.toFixed(1) + '%'
+}
+else {
+    
+}
+
+
+
+document.querySelector('.jshstar_b1').textContent = collectedstars_b1 + '/'+ totalstars_b1
+document.querySelector('.jshstar_b2').textContent = collectedstars_b2 + '/'+ totalstars_b2
+document.querySelector('.jshstar_e1').textContent = collectedstars_e1 + '/'+ totalstars_e1
+document.querySelector('.jshstar_e2').textContent = collectedstars_e2 + '/'+ totalstars_e2
+document.querySelector('.jshstar_m1').textContent = collectedstars_m1 + '/'+ totalstars_m1
+document.querySelector('.jshstar_m2').textContent = collectedstars_m2 + '/'+ totalstars_m2
+document.querySelector('.jshstar_p1').textContent = collectedstars_p1 + '/'+ totalstars_p1
+document.querySelector('.jshstar_p2').textContent = collectedstars_p2 + '/'+ totalstars_p2
+document.querySelector('.jshstar_c1').textContent = collectedstars_c1 + '/'+ totalstars_c1
+document.querySelector('.jshstar_c2').textContent = collectedstars_c2 + '/'+ totalstars_c2
+document.querySelector('.jshstar_bi1').textContent = collectedstars_bi1 + '/'+ totalstars_bi1
+document.querySelector('.jshstar_bi2').textContent = collectedstars_bi2 + '/'+ totalstars_bi2
+document.querySelector('.jshstar_ict').textContent = collectedstars_ict + '/'+ totalstars_ict
+document.querySelector('.jshper_b1').textContent = percentage_b1.toFixed(1) + '%'
+document.querySelector('.jshper_b2').textContent = percentage_b2.toFixed(1) + '%'
+document.querySelector('.jshper_e1').textContent = percentage_e1.toFixed(1) + '%'
+document.querySelector('.jshper_e2').textContent = percentage_e2.toFixed(1) + '%'
+document.querySelector('.jshper_m1').textContent = percentage_m1.toFixed(1) + '%'
+document.querySelector('.jshper_m2').textContent = percentage_m2.toFixed(1) + '%'
+document.querySelector('.jshper_p1').textContent = percentage_p1.toFixed(1) + '%'
+document.querySelector('.jshper_p2').textContent = percentage_p2.toFixed(1) + '%'
+document.querySelector('.jshper_c1').textContent = percentage_c1.toFixed(1) + '%'
+document.querySelector('.jshper_c2').textContent = percentage_c2.toFixed(1) + '%'
+document.querySelector('.jshper_bi1').textContent = percentage_bi1.toFixed(1) + '%'
+document.querySelector('.jshper_bi2').textContent = percentage_bi2.toFixed(1) + '%'
+document.querySelector('.jshper_ict').textContent = percentage_ict.toFixed(1) + '%'
+
+
+
+
+
+
+
